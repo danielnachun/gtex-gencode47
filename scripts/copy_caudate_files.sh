58a4e59e17ab24947ea32049c341e77ef4ccc275
Kate Lawrence <klawren@stanford.edu>
2025-03-28 09:48:06 -0700

    split analysis

--- scripts/copy_caudate_files.sh
+++ scripts/copy_caudate_files.sh
@@ -1,6 +1,9 @@
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/coverage gs://fa-exchange2/caudate_analysis/coverage
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/flagstat gs://fa-exchange2/caudate_analysis/flagstat
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/gatk gs://fa-exchange2/caudate_analysis/gatk
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/leafcutter gs://fa-exchange2/caudate_analysis/leafcutter
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/rnaseq_qc gs://fa-exchange2/caudate_analysis/rnaseq_qc
-gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/rsem gs://fa-exchange2/caudate_analysis/rsem
+#!/bin/bash
+set -e
+
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/coverage gs://fa-exchange2/caudate_analysis/coverage || exit 1
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/flagstat gs://fa-exchange2/caudate_analysis/flagstat || exit 1
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/gatk gs://fa-exchange2/caudate_analysis/gatk || exit 1
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/leafcutter gs://fa-exchange2/caudate_analysis/leafcutter || exit 1
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/rnaseq_qc gs://fa-exchange2/caudate_analysis/rnaseq_qc || exit 1
+gsutil -m cp -r /oak/stanford/groups/smontgom/dnachun/data/gtex/v10/output/caudate_analysis/rsem gs://fa-exchange2/caudate_analysis/rsem || exit 1
